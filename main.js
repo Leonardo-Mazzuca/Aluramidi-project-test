6db088e27578dfbb1733016bc4419f1d509735ed
Leonardo-Mazzuca <124372925+Leonardo-Mazzuca@users.noreply.github.com>
2023-02-07 15:39:17 -0300

    Aluramidi-project archives

---
+++ main.js
@@ -0,0 +1,43 @@
+
+function tocaSom(seletorAudio) {
+      const elemento = document.querySelector(seletorAudio);
+
+
+     if (elemento && elemento.LocalName === 'audio') {
+          elemento.play();
+
+     }
+     else{
+          console.log('Elemento  ou seletor invalido');
+     }
+     
+}
+
+
+const ListaDeTeclas = document.querySelectorAll('.tecla');
+
+for  (let contador = 0; contador < ListaDeTeclas.length; contador++ ) {
+
+     const tecla = ListaDeTeclas[contador]
+     const instrumento = tecla.classList[1];
+     const idAudio = `#som_${instrumento}`;
+
+     
+     tecla.onclick = function () {
+          tocaSom(idAudio);
+     }
+
+     tecla.onkeydown = function (evento) {
+
+          if (evento.code === 'Space' || evento.code === 'Enter' ) {
+               tecla.classList.add('ativa');
+          }
+
+     }
+
+     tecla.onkeyup = function () {
+          tecla.classList.remove('ativa');
+     }
+
+
+}
